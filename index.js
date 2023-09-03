2d3d8b06af4a075df24633fdfe096af5fb0860db
ingvr94 <ingvar94@mail.ru>
2023-09-03 18:05:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const plus =document.querySelectorAll('.fold')
+const hug = document.querySelectorAll('.hug')
+
+const objects=['Пирс','Офис','Cтадион','Вход','Отель','Вокзал','Ресторан','Дом','Медцентр','Дом']
+
+Array.from(plus).forEach((e,i)=>{
+    e.addEventListener('click',()=>{
+        e.classList.toggle('hug')
+       if (e.textContent.includes('-')){
+         e.textContent='+'
+        }
+        else {
+            setTimeout(()=>e.textContent=`- ${objects[i]}`,200)
+
+        }
+    })
+})
